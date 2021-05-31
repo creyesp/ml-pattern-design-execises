e80b3af4c8e784f46bc06856e32a6831162e28f2
creyesp-peya <cesar.reyes@pedidosya.com>
2021-05-30 23:16:14 -0300

    first version

---
+++ tensorflow_tabular/ds.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import tensorflow as tf
+
+from config import NUMERICAL_COLUMNS, CATEGORICAL_COLUMNS, COLUMNS, TARGET
+
+
+def load_dataset(file_path):
+    data = pd.read_csv(file_path)
+    data.dropna(subset=COLUMNS, inplace=True)
+    #pd.concat([pd.read_csv(f'/home/creyesp/tmp/tabular/data/{file_}') for file_ in os.listdir('/home/creyesp/tmp/tabular/data/') if file_.endswith('gz')]).reset_index(drop=True)
+
+    dataset = data[COLUMNS]
+    target_class = (data[TARGET]==0).astype(int)
+    return dataset, target_class
+
+
+def split_dataset(features, target):
+    x_train, x_test, y_train, y_test = \
+        train_test_split(features, target, test_size=0.1, stratify=target)
+    
+    print(f'number of clases: {y_train.value_counts()}')
+    print(f'number of clases: {y_test.value_counts()}')
+    return x_train, x_test, y_train, y_test
+
+
+def initial_bias(target):
+    pos = (target).sum()
+    neg = (target==0).sum()
+    initial_bias = np.log([pos/neg])[0]
+    return initial_bias
+
+
+def describe_dataset(features, target):
+    num_desc = features[NUMERICAL_COLUMNS].apply(lambda x: {'mean': x.mean(), 'variance': x.var()})
+    cat_desc = features[CATEGORICAL_COLUMNS].apply(lambda x: {'vocabulary': x.unique()})
+    bias = initial_bias(target)
+    description = {
+        'numerical': dict(num_desc),
+        'categorical': dict(cat_desc),
+        'bias': bias
+    }
+    return description
+
+def tf_dataset(features, target, train=False):
+    ds_train = tf.data.Dataset.from_tensor_slices((dict(features), target))
+
+    if train:
+        ds_train = ds_train.shuffle(buffer_size=1_00_000)
+    
+    ds_train = ds_train.batch(2048, num_parallel_calls=tf.data.AUTOTUNE)
+    
+    return ds_train
+
+def get_dataset(file_path):
+    features, target = load_dataset(file_path)
+    x_train, x_test, y_train, y_test = split_dataset(features, target)
+    data_description = describe_dataset(x_train, y_train)
+    
+    ds_train = tf_dataset(x_train, y_train, train=True)
+    ds_test = tf_dataset(x_test, y_test, train=False)
+
+    return ds_train, ds_test, data_description
---
+++ tensorflow_tabular/model.py
@@ -0,0 +1,120 @@
+#%%
+import sys
+sys.path.insert(0, "/home/creyesp/Projects/repos/personal/training/tensorflow_tabular")
+#%%
+import os
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Embedding, Flatten, Dense, concatenate
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras.models import Model
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import numpy as np
+import pandas as pd
+
+import ds
+import config
+
+
+METRICS = [
+      tf.keras.metrics.TruePositives(name='tp'),
+      tf.keras.metrics.FalsePositives(name='fp'),
+      tf.keras.metrics.TrueNegatives(name='tn'),
+      tf.keras.metrics.FalseNegatives(name='fn'), 
+      tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+      tf.keras.metrics.Precision(name='precision'),
+      tf.keras.metrics.Recall(name='recall'),
+      tf.keras.metrics.AUC(name='auc'),
+      tf.keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+]
+#%%
+file_path = '/home/creyesp/tmp/tabular/data/commit_tabular_000000000000.csv.gz'
+ds_train, ds_test, data_description = ds.get_dataset(file_path)
+
+#%%
+numerical_inputs = {}
+numerical_features = []
+
+# Numeric features.
+for column in config.NUMERICAL_COLUMNS:
+    print(column)
+    mean = data_description['numerical'][column]['mean']
+    variance = data_description['numerical'][column]['variance']
+    
+    numeric_col = tf.keras.Input(shape=(1,), name=f'input_{column}')
+    normalization_layer = preprocessing.Normalization(name=f'layer_{column}', mean=mean, variance=variance)
+    print(f'layer name: {normalization_layer.name}')
+    encoded_numeric_col = normalization_layer(numeric_col)
+    numerical_inputs[column] = numeric_col
+    numerical_features.append(encoded_numeric_col)
+
+
+#%%
+categorical_inputs = {}
+categorical_features = []
+
+cat_col = ['country_id', 'day_of_week_order', 'order_hour', 'business_type']
+for column in cat_col:
+    print(column)
+    input_col = tf.keras.Input(shape=(1,), name=f'input_{column}', dtype='int32')
+    # category_encoding_layer = get_category_encoding_layer(cat, ds_train, 'int32')
+    vocabulary = data_description['categorical'][column]['vocabulary']
+    category_encoding_layer = preprocessing.IntegerLookup(name=f'layer_{column}', vocabulary=vocabulary, output_mode='binary', mask_token=-1, oov_token=-2)
+    print(f'layer name: {category_encoding_layer.name}')
+    encoded_cat_col = category_encoding_layer(input_col)
+    categorical_inputs[column] = input_col
+    categorical_features.append(encoded_cat_col)
+
+#%%
+embedding_inputs = {}
+embedding_features = []
+
+for column in ['restaurant_id']:
+    input_embedding = tf.keras.Input(shape=(1,), name=f'input_{column}', dtype='int32')
+    vocabulary = data_description['categorical'][column]['vocabulary']
+    restaurant_id_type = tf.feature_column.categorical_column_with_vocabulary_list(column, vocabulary_list=vocabulary)
+    restaurant_id_embedding = tf.feature_column.embedding_column(restaurant_id_type, dimension=64)
+    embedding_inputs[column] = input_embedding
+    embedding_features.append(restaurant_id_embedding)
+embedding_layer = tf.keras.layers.DenseFeatures(embedding_features)(embedding_inputs)
+
+    
+
+#%%
+initial_bias = data_description['bias']
+inputs = {}
+inputs.update(embedding_inputs)
+inputs.update(categorical_inputs)
+inputs.update(numerical_inputs)
+
+feature_layer = tf.keras.layers.concatenate(categorical_features + numerical_features + [embedding_layer])
+dense_layer = tf.keras.layers.Dense(128, activation='relu')(feature_layer)
+dense_layer = tf.keras.layers.Dense(128, activation='relu')(dense_layer)
+output_bias = tf.keras.initializers.Constant([initial_bias])
+output = tf.keras.layers.Dense(1, activation='sigmoid', bias_initializer=output_bias)(feature_layer)
+
+model = tf.keras.models.Model(inputs=inputs, outputs=output)
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=METRICS)
+model
+
+#%% 
+tf.keras.utils.plot_model(model, show_shapes=False)
+
+#%%
+history = model.fit(ds_train,
+          validation_data=ds_test,
+          epochs=1,)
+#%%
+print(history.history)
+#%%
+#pred_y_train = model.predict(ds_train)
+pred_y_test = model.predict(ds_test)
+#%%
+pred_y_test.shape
+#%%
+y_test = [k for batch in ds_test.map(lambda x, y: y).as_numpy_iterator() for k in batch]
+#%%
+cm = confusion_matrix(y_test,pred_y_test.squeeze()>0.5)
+print(cm)
+ConfusionMatrixDisplay(cm).plot()
+
+# %%
